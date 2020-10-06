1959f19efdb0ad7bab3cae72c9bcd1e1dcfdb596
Lindsay Drotos <lindsaydrotos@gmail.com>
2020-10-05 21:04:16 -0700

    DRY updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles  # provides all REST-ful routes to Rails resources
+ # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] #We built this manually at first, now encompassed in line above
 end
+
+# REST - Representational State Transfer - mapping HTTP verbs (get, post, put/patch, delete) to CRUD actions
