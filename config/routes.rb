d12b4e15f7bca184213747ebb09fd590beaa9dcc
Lindsay Drotos <lindsaydrotos@gmail.com>
2021-02-01 21:44:34 -0700

    Added admin functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] #We built this manually at first, now encompassed in line above
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
 
 # REST - Representational State Transfer - mapping HTTP verbs (get, post, put/patch, delete) to CRUD actions
